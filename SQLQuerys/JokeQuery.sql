0d5bdd435b88194428ee921201dcfc6ba3cc4746
Aleksander Stęplewski <56417131+ElStempel@users.noreply.github.com>
2021-01-11 12:34:26 +0100

    Update issue #18

--- SQLQuerys/JokeQuery.sql
+++ SQLQuerys/JokeQuery.sql
@@ -1,13 +1,15 @@
 CREATE TABLE Joke(
 	ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	CreatorID integer,
 	Content varchar(255)
 );
 
 INSERT INTO Joke (Content)
-VALUES ('Gdzie podpisano traktat wersalski? \n- Na samym dole, pod tekstem.'),
-('Co sie po jednej stronie glaszcze a po drugiej lize?? - Nie wiem. -... znaczek pocztowy!'),
-('Co sie stanie jak walec drogowy przejedzie czlowieka? - Konwersja obrazu z 3D na 2D'),
-('Jak sie nazywa mnich odpowiedzialny za podatki? - Brat PIT.'),
-('Jaka jest ulubiona zabawa dzieci grabarza?  - W chowanego.'),
-('Co robi elektryk na scenie? - Buduje napiecie.'),
-('Jaki jest ulubiony owoc zolnierza? - Granat.');
+VALUES (1, 'Gdzie podpisano traktat wersalski? \n- Na samym dole, pod tekstem.'),
+(1, 'Co sie po jednej stronie glaszcze a po drugiej lize?? - Nie wiem. -... znaczek pocztowy!'),
+(1, 'Co sie stanie jak walec drogowy przejedzie czlowieka? - Konwersja obrazu z 3D na 2D'),
+(1, 'Jak sie nazywa mnich odpowiedzialny za podatki? - Brat PIT.'),
+(1, 'Jaka jest ulubiona zabawa dzieci grabarza?  - W chowanego.'),
+(1, 'Co robi elektryk na scenie? - Buduje napiecie.'),
+(1, 'Przychodzi baba do lekarza, a lekarz tez baba.'),
+(1, 'Jaki jest ulubiony owoc zolnierza? - Granat.');
